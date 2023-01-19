1ba417cd41a21300d73731e8bf987c91b9df77e4
yanxhz <adrianduha123@gmail.com>
2023-01-19 14:53:34 +0700

    update login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login  extends Component {
+    
+    render() { 
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                        <div className="col-md-6">
+                        <h2>Login Sistem</h2>
+                            <form>
+                                <div className="form-group">
+                                    <label for="exampleInputEmail1">Username </label>
+                                     <input type="email" className="form-control" placeholder="Masukan username anda"  required/>
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" placeholder="Password" />
+                                </div>
+                                <div class="form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>   
+                            </form>
+                        </div>
+                </div>
+            </div> 
+         );
+    }
 }
-
-export default App;
+ 
+export default Login ;
